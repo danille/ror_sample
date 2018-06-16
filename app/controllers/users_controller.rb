6210b4f300ae88b0267c4a45eeac4699fb78d32b
Danylo Lebediev <danilleock@gmail.com>
2018-06-16 18:53:37 +0200

    Added simple sign up.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,22 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # HAndling successful saving
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:name).permit(:name, :email, :password, :password_confirmation)
+    end
+  end
+
 end
