2b62727154e726b3d12de5a019e31ac9cb1796a4
Danylo Lebediev <danilleock@gmail.com>
2018-07-03 14:07:10 +0200

    Added friendly redirect after logging in

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -17,10 +17,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select "div.alert", "The form contains 4 errors."
   end
 
-  test 'successfull edit' do
-    log_in_as(@user)  
+  test 'successfull edit with friendly redirect' do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)  
+    assert_nil session[:forwarding_url]
     name = 'Foo Bar'
     email = 'foo@bar.com'
     patch user_path(@user), params: { user: { name: name, 
